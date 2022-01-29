f632fd6387d475e7f3e84a38e6fbff6ddfa99a32
Pavan Palnati <97885128+Pavan-Palnati@users.noreply.github.com>
2022-01-29 17:59:35 +0530

    Add files via upload

---
+++ Swapping of numbers.js
@@ -0,0 +1,19 @@
+var a = 30
+var b = 100
+/*
+With temp 
+var temp = a
+var a = b 
+var b = temp 
+
+OR 
+
+With out temp:
+*/
+
+var a = a+b 
+var b = a-b 
+var a = a-b
+
+
+console.log("After swaping: a = ",a," b = ", b )
